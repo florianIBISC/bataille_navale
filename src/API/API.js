251ca308ede4324e2fc6321d9c2368a11f7b2b5b
echappe <45846749+echappe@users.noreply.github.com>
2020-01-14 15:10:40 +0100

    Add files via upload

---
+++ src/API/API.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const headers = {
+    "Content-Type": "application/json"
+};
+const burl = "http://192.168.194.198:8080"; //
+
+export default {
+    login: function(pseudo, password){
+        return axios.post(`${burl}/users/login`,
+        {
+            pseudo,
+            password,
+        },{
+            headers: headers
+        });
+    }
+}
