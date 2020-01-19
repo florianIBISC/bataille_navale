861ee13fc84f76e8caa55915f3b9d5d03cb01cb7
echappe <45846749+echappe@users.noreply.github.com>
2020-01-19 17:37:56 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Switch,Route } from 'react-router-dom';  //Navigation
 import Connection from './Authentification/Connexion';
 import Compte from './Authentification/Compte'
+import Menu from './Authentification/Menu'
 
 import Intialisation from './Jeu/Initialisation'
-import Jeu from './Jeu/Jeu';
+//import Jeu from './Jeu/Jeu';
+import Jeu from './Jeu/Play';
 import './App.css';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
     <React.Fragment>
       <Switch>
         <Route exact path='/' component={Connection}/>
+        <Route path='/menu' component={Menu}/>
         <Route path='/compte' component={Compte}/>
         <Route path='/initalisation' component={Intialisation}/>
         <Route path='/jeu' component={Jeu}/>
